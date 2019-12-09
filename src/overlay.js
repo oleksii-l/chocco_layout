f8b0e4ff3bbe359d8dedf07af07c90ad07ccb144
Oleksii Leshchenko <oleksii.leshchenko@gmail.com>
2019-12-09 18:38:46 +0200

    full-screen menu finished

---
+++ src/overlay.js
@@ -0,0 +1,20 @@
+// простой способ
+let burger  = document.querySelector('.burger');
+let overlay = document.querySelector('.overlay');
+let body = document.querySelector('body');
+
+let links = document.querySelectorAll('.over-menu__link');
+
+links.forEach(function(element){
+  element.addEventListener('click' , toggleMenu);
+})
+
+function toggleMenu(){
+  burger.classList.toggle('burger--active');
+  overlay.classList.toggle('overlay--active');
+  body.classList.toggle('body--active-menu');
+}
+
+burger.addEventListener('click' , toggleMenu);
+
+
